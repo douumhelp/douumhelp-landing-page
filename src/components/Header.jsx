9b23309ae0353aa4e3ef7ea3cb1e5599cbffcbdd
Matheus Souza <szmatheussouza@gmail.com>
2025-01-01 17:16:44 -0300

    header concluido

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,17 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="bg-[#242424] min-h-screen text-white">
-    {/* Header */}
-    <header className="flex justify-between items-center p-4 bg-[#242424] shadow-md fixed w-full z-10">
-      <img src="/img/logo-white.png" alt="Logo" className="h-12 ml-4"/>
+<div className="bg-[#242424] min-h-screen text-white">
+  {/* Header */}
+  <header className="flex justify-between items-center p-4 bg-[#242424] shadow-md fixed w-full z-10">
+    <img src="/img/logo-white.png" alt="Logo" className="w-20 h-20 object-contain" style={{ marginLeft: '15rem' }}/>
+    <div className="space-x-5">
+        <button className="text-black py-2 px-4 rounded-full" style={{ backgroundColor: '#FFE600' }}>Preciso de Ajuda</button>
+        <button className="text-black py-2 px-4 rounded-full" style={{ backgroundColor: '#FFE600' }}>Quero oferecer serviços</button>
+    </div>
+  </header>
+</div>
 
-    </header>
-  </div>
 );
 
 }
